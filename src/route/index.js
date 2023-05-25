70ada988a4f39d858cd825f0d4cda89216470e5c
RomaPranichuk <pranicukroman766@gmail.com>
2023-05-25 21:13:01 +0300

    some upd on master 6

--- src/route/index.js
+++ src/route/index.js
@@ -2121,5 +2121,21 @@ router.get('/shoporder', function (req, res) {
 
 // ================================================================
 
+// router.get Створює нам один ентпоїнт
+
+//           ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/bootstrap5', function (req, res) {
+    // res.render генерує нам HTML сторінку
+
+    //            ↙ cюди вводимо назву файлу з сontainer
+    res.render('bootstrap5', {
+        layout: 'bootstrap',
+    })
+
+    //                  ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
 // Підключаємо роутер до бек-енду
 module.exports = router
